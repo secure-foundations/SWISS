4d624d52ee13721de5aab2b31dd394061a7de5aa
Travis Hance <tjhance7@gmail.com>
2019-04-25 13:30:19 -0400

    add top_quantifier_desc

---
+++ src/top_quantifier_desc.cpp
@@ -0,0 +1,117 @@
+#include "top_quantifier_desc.h"
+
+#include <set>
+#include <cassert>
+
+using namespace std;
+
+TopQuantifierDesc::TopQuantifierDesc(value v) {
+  while (true) {
+    if (Forall* f = dynamic_cast<Forall*>(v.get())) {
+      for (VarDecl decl : f->decls) {
+        d.push_back(make_pair(QType::Forall, vector<VarDecl>{decl}));
+      }
+      v = f->body;
+    } else if (NearlyForall* f = dynamic_cast<NearlyForall*>(v.get())) {
+      d.push_back(make_pair(QType::NearlyForall, f->decls));
+      v = f->body;
+    } else {
+      break;
+    }
+  }
+}
+
+vector<VarDecl> TopQuantifierDesc::decls() const {
+  vector<VarDecl> res;
+  for (auto p : d) {
+    for (auto decl : p.second) {
+      res.push_back(decl);
+    }
+  }
+  return res;
+}
+
+vector<QRange> TopQuantifierDesc::with_foralls_grouped() const {
+  vector<QRange> res;
+  QRange cur;
+  cur.start = 0;
+  cur.end = 0;
+
+  for (auto p : d) {
+    QType ty = p.first;
+    if (ty == QType::Forall) {
+      cur.end++;
+      cur.decls.push_back(p.second[0]);
+      cur.qtype = ty;
+    } else if (ty == QType::NearlyForall) {
+      if (cur.end > cur.start) {
+        res.push_back(cur);
+      }
+
+      cur.start = cur.end;
+      cur.decls = p.second;
+      cur.end = cur.start + cur.decls.size();
+      cur.qtype = ty;
+      res.push_back(cur);
+
+      cur.start = cur.end;
+      cur.decls.clear();
+    } else {
+      assert(false);
+    }
+  }
+
+  if (cur.end > cur.start) {
+    res.push_back(cur);
+  }
+
+  return res;
+}
+
+vector<QSRange> TopQuantifierDesc::grouped_by_sort() const {
+  vector<QSRange> res;
+  QSRange cur;
+  cur.start = 0;
+  cur.end = 0;
+
+  for (auto p : d) {
+    QType ty = p.first;
+    for (auto decl : p.second) {
+      if (cur.end > cur.start && !(cur.qtype == ty && sorts_eq(cur.decls[0].sort, decl.sort))) {
+        res.push_back(cur);
+        cur.start = cur.end;
+        cur.decls.clear();
+      }
+
+      cur.decls.push_back(decl);
+      cur.qtype = ty;
+      cur.sort = decl.sort;
+      cur.end++;
+    }
+
+    if (ty == QType::NearlyForall && cur.end > cur.start) {
+      res.push_back(cur);
+      cur.start = cur.end;
+      cur.decls.clear();
+    }
+  }
+
+  if (cur.end > cur.start) {
+    res.push_back(cur);
+  }
+
+  set<string> names;
+  for (int i = 0; i < res.size(); i++) {
+    if (i > 0 && res[i].qtype != res[i-1].qtype) {
+      names.clear();
+    }
+    UninterpretedSort* usort = dynamic_cast<UninterpretedSort*>(res[i].decls[0].sort.get());
+    assert(usort != NULL);
+    if (res[i].qtype == QType::Forall) {
+      assert (names.count(usort->name) == 0);
+    }
+    names.insert(usort->name);
+  }
+
+  return res;
+}
