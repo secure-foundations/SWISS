f74cd6ca7e1324ce6bd678f5711bc09cdf72e605
Travis Hance <tjhance7@gmail.com>
2019-11-08 10:53:35 -0500

    benchmarks

---
+++ bench.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo_and_run() { echo "$*" ; "$@" ; }
+
+if [ "$1" = "naive-leader-election" ]; then
+  echo_and_run ./save.sh benchmarks/leader-election.ivy --enum-naive --conj-arity 3 --disj-arity 3
+fi
+
+if [ "$1" = "naive-paxos-missing1" ]; then
+  echo_and_run ./save.sh benchmarks/paxos_epr_missing1.ivy --enum-naive --impl-shape --disj-arity 3 --with-conjs 
+fi
+
+
+
+if [ "$1" = "sat-leader-election" ]; then
+  echo_and_run ./save.sh benchmarks/leader-election.ivy --enum-sat --arity 3 --depth 4
+fi
+
+
+
+if [ "$1" = "sat-inc-leader-election" ]; then
+  echo_and_run ./save.sh benchmarks/leader-election.ivy --incremental --enum-sat --arity 3 --depth 3
+fi
+
+if [ "$1" = "sat-inc-learning-switch" ]; then
+  echo_and_run ./save.sh benchmarks/learning-switch.ivy --incremental --enum-sat --arity 3 --depth 3
+fi
+
