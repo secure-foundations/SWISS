ba5323ed8e5e717ac825c33535073290ed959cd7
Travis Hance <tjhance7@gmail.com>
2020-01-27 11:14:23 -0500

    fix parallel command

--- async_benchmarks.sh
+++ async_benchmarks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 BENCHES=$(python print_all_benchmark_names.py)
-parallel "$@" -k ./run_benchmarks.py ::: $BENCHES
+#parallel "$@" -k ./run_benchmarks.py ::: $BENCHES
+parallel "$@" ./run_benchmarks.py -- $BENCHES
