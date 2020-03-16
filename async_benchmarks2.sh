d485a3dac6350bbc9761c8d6cae2d3af3d3d2e79
Travis Hance <tjhance7@gmail.com>
2020-03-16 11:24:50 -0400

    modify benchmark script

--- async_benchmarks2.sh
+++ async_benchmarks2.sh
@@ -1,22 +1,22 @@
-parallel "$@" ./run_benchmarks.py ::: \
-  "naive-strat2-breadth-paxos-4-r3" \
-  "naive-strat2-breadth-paxos-4-r3 --pre-bmc --post-bmc" \
-  "naive-strat2-breadth-paxos-4-r3 --minimal-models" \
-  "naive-strat2-breadth-paxos-4-r3 --minimal-models --pre-bmc --post-bmc" \
-  "naive-breadth-paxos-exist-2" \
-  "naive-breadth-paxos-exist-2 --pre-bmc --post-bmc" \
-  "naive-breadth-paxos-exist-2 --minimal-models" \
-  "naive-breadth-paxos-exist-2 --minimal-models --pre-bmc --post-bmc" \
-  "naive-breadth-paxos-exist-1" \
-  "naive-breadth-paxos-exist-1 --pre-bmc --post-bmc" \
-  "naive-breadth-paxos-exist-1 --minimal-models" \
-  "naive-breadth-paxos-exist-1 --minimal-models --pre-bmc --post-bmc" \
-  "naive-full-paxos-exist-1" \
-  "naive-full-paxos-exist-1 --pre-bmc --post-bmc" \
-  "naive-full-paxos-exist-1 --minimal-models" \
-  "naive-full-paxos-exist-1 --minimal-models --pre-bmc --post-bmc" \
-  "naive-full-paxos-exist-1-solve" \
-  "naive-strat2-breadth-learning-switch" \
-  "naive-strat2-breadth-learning-switch --pre-bmc --post-bmc" \
-  "naive-strat2-breadth-learning-switch --minimal-models" \
-  "naive-strat2-breadth-learning-switch --minimal-models --pre-bmc --post-bmc" \
+parallel "$@" ::: \
+  "./run_benchmarks.py naive-strat2-breadth-paxos-4-r3" \
+  "./run_benchmarks.py naive-strat2-breadth-paxos-4-r3 --pre-bmc --post-bmc" \
+  "./run_benchmarks.py naive-strat2-breadth-paxos-4-r3 --minimal-models" \
+  "./run_benchmarks.py naive-strat2-breadth-paxos-4-r3 --minimal-models --pre-bmc --post-bmc" \
+  "./run_benchmarks.py naive-breadth-paxos-exist-2" \
+  "./run_benchmarks.py naive-breadth-paxos-exist-2 --pre-bmc --post-bmc" \
+  "./run_benchmarks.py naive-breadth-paxos-exist-2 --minimal-models" \
+  "./run_benchmarks.py naive-breadth-paxos-exist-2 --minimal-models --pre-bmc --post-bmc" \
+  "./run_benchmarks.py naive-breadth-paxos-exist-1" \
+  "./run_benchmarks.py naive-breadth-paxos-exist-1 --pre-bmc --post-bmc" \
+  "./run_benchmarks.py naive-breadth-paxos-exist-1 --minimal-models" \
+  "./run_benchmarks.py naive-breadth-paxos-exist-1 --minimal-models --pre-bmc --post-bmc" \
+  "./run_benchmarks.py naive-full-paxos-exist-1" \
+  "./run_benchmarks.py naive-full-paxos-exist-1 --pre-bmc --post-bmc" \
+  "./run_benchmarks.py naive-full-paxos-exist-1 --minimal-models" \
+  "./run_benchmarks.py naive-full-paxos-exist-1 --minimal-models --pre-bmc --post-bmc" \
+  "./run_benchmarks.py naive-full-paxos-exist-1-solve" \
+  "./run_benchmarks.py naive-strat2-breadth-learning-switch" \
+  "./run_benchmarks.py naive-strat2-breadth-learning-switch --pre-bmc --post-bmc" \
+  "./run_benchmarks.py naive-strat2-breadth-learning-switch --minimal-models" \
+  "./run_benchmarks.py naive-strat2-breadth-learning-switch --minimal-models --pre-bmc --post-bmc" \
--- run_benchmarks.py
+++ run_benchmarks.py
@@ -10,8 +10,8 @@ from benchmarks import BENCHMARKS
 HOUR = 60*60
 TIMEOUT = 4 * HOUR
 
-def run_benchmark(name):
-  proc = subprocess.Popen(["./bench.sh", name],
+def run_benchmark(name, args):
+  proc = subprocess.Popen(["./bench.sh", name] + args,
       preexec_fn=os.setsid,
       stdout=subprocess.PIPE,
       stderr=subprocess.PIPE)
@@ -61,8 +61,9 @@ def pad_left(name, l):
     name = " " + name
   return name
 
-def benchmark_print_1(name):
-  return pad_right(name + " ...", 50)
+def benchmark_print_1(name, args = []):
+  cmd = " ".join([name] + args)
+  return pad_right(cmd + " ...", 50)
 
 def benchmark_print_2(name, res, logfile):
   return " " + pad_left(res, 16) + "   [" + logfile + "]"
@@ -70,15 +71,19 @@ def benchmark_print_2(name, res, logfile):
 def benchmark_print_line(name, res, logfile):
   return benchmark_print_1(name) + benchmark_print_2(name, res, logfile)
 
-def do_benchmark(name):
-  logfile, res = run_benchmark(name)
-  print(benchmark_print_1(name) + benchmark_print_2(name, res, logfile))
+def do_benchmark(name, args):
+  logfile, res = run_benchmark(name, args)
+  print(benchmark_print_1(name, args) +
+        benchmark_print_2(name, res, logfile))
 
 if __name__ == '__main__':
-  benches = sys.argv[1:]
-  if len(benches) == 0:
-    for bench in BENCHMARKS:
-      do_benchmark(bench)
-  else:
-    for bench in benches:
-      do_benchmark(bench)
+  #benches = sys.argv[1:]
+  #if len(benches) == 0:
+  #  for bench in BENCHMARKS:
+  #    do_benchmark(bench)
+  #else:
+  #  for bench in benches:
+  #    do_benchmark(bench)
+  bench = sys.argv[1]
+  args = sys.argv[2:]
+  do_benchmark(bench, args)
