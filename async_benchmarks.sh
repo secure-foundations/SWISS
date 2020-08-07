408efa974d6bbc2124afaaa9f1f7c0217a41df6b
Travis Hance <tjhance7@gmail.com>
2020-08-07 09:30:36 -0400

    clean out some junk

--- async_benchmarks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-BENCHES=$(python print_all_benchmark_names.py)
-#parallel "$@" -k ./run_benchmarks.py ::: $BENCHES
-parallel "$@" ./run_benchmarks.py -- $BENCHES
--- print_benchmark_args.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-from benchmarks import BENCHMARKS
-import sys
-
-def print_args(benchmark, extra):
-  print(benchmark.ivy_file, " ".join(extra + list(benchmark.args)))
-
-if __name__ == '__main__':
-  print_args(BENCHMARKS[sys.argv[1]], sys.argv[2:])
