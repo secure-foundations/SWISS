d47fa54bda1cfbf865b56233c9578a29b22e2678
Travis Hance <tjhance7@gmail.com>
2021-04-19 20:05:24 -0400

    add get-deps.sh script

---
+++ get-deps.sh
@@ -0,0 +1,17 @@
+git clone git@github.com:secure-foundations/CVC4-fork.git cvc4
+cd cvc4
+# swiss-fork branch
+git checkout 2f3d441727d87db35bce38a99c79c0b19c4fd1ef
+cd ..
+
+git clone git@github.com:secure-foundations/mypyvy-fork.git mypyvy
+cd mypyvy
+# swiss-fork branch
+git checkout a3a386245eaea442fdcba866a38a89e96fc7e2e6
+cd ..
+
+git clone git@github.com:secure-foundations/ivy-fork.git ivy
+cd ivy
+# swiss-fork branch
+git checkout 74e6ee922c965a15a0f4f5d5ae60dc85acc64850
+cd ..
